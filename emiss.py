93c214e048bd7f55e320ae9e408007abb3b9e9a7
Tom Douglas <pytd@leeds.ac.uk>
2014-08-14 14:57:45 +0100

    fixed

--- emiss.py
+++ emiss.py
@@ -8,12 +8,12 @@ def emiss(npls,t , nu):
     g=[gaunt(t,z,nu) for z in [1,2,3,6]]# gaunt factors are of order unity 
     npl1,npl2,npl3,npl6=npls
     ne=npl1 + 2*npl2 + 3*npl3 + 6*npl6
-    hnukt = 4.801449e-11*nu/t #(h nu) / (k_b T)
+    hnukt = cns.Planck*nu/(cns.Boltzmann*t) #(h nu) / (k_b T)
     epsff = (6.8e-38*exp(-hnukt)*ne*(g[0]*npl1+g[1]*4.0*npl2+g[2]*9.0*npl3+g[3]*36.0*npl6))/sqrt(t) # ff emission co-efficient from rybiki & lightman 5.14b
     epsff /=(4*pi)
     kapff = (0.01765/((nu*nu)*pow(t,1.5)))*ne*(g[0]*npl1 + g[1]*4.0*npl2 + g[2]*9.0*npl3) # ff absorption co-efficient from rybiki & lightman 5.19b
 
-    return epsff,kapff
+    return epsff,kapff,g
 
 def eDensity(rho,t):
 
